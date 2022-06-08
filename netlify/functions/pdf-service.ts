084c77014118ba80d79ee7c207f31c0c55d4af82
Markus Wohlan <markuswohlan@gmail.com>
2022-06-08 14:36:07 +0200

    fix pdf-service

--- netlify/functions/pdf-service.ts
+++ netlify/functions/pdf-service.ts
@@ -1,10 +1,9 @@
 
 import { Handler } from '@netlify/functions';
 const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
 
 exports.handler = async function(event, context) {
-  const browser = await puppeteer.launch({
+  const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
     headless: true,
