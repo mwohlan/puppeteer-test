77c4e0aa41d769d30504cf4e41402b4b7e4c2493
Markus Wohlan <markuswohlan@gmail.com>
2022-06-08 14:17:16 +0200

    pdf-service

--- netlify/functions/pdf-service.ts
+++ netlify/functions/pdf-service.ts
@@ -1,10 +1,28 @@
-import { Handler } from "@netlify/functions";
 
-const handler: Handler = async (event, context) => {
-  return {
+import { Handler } from '@netlify/functions';
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+exports.handler = async function(event, context) {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+
+   await page.goto('https://timelino.vercel.app/', { waitUntil: 'load' })
+  const pdf = await page.pdf({ format: 'A4' })
+
+  await page.close()
+
+    return {
+      
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+      body: JSON.stringify({
+          status: 'Ok',
+          pdf
+      })
   };
-};
-
-export { handler };
+} as Handler
